288684349fec74c1b6a4e3e6430944a5ca35e0ab
islavstan <islavstan@gmail.com>
2017-05-26 14:09:03 +0300

    add SwagPoints

---
+++ app/src/main/java/com/islavstan/customviewexample/DurationTextView.java
@@ -0,0 +1,22 @@
+package com.islavstan.customviewexample;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * Created by islav on 24.05.2017.
+ */
+
+public class DurationTextView extends TextView {
+
+
+    public DurationTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+
+}
+
+
+
--- app/src/main/java/com/islavstan/customviewexample/LovelyView.java
+++ app/src/main/java/com/islavstan/customviewexample/LovelyView.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Rect;
+import android.support.constraint.solver.widgets.Rectangle;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -20,43 +22,45 @@ public class LovelyView extends View {
     private Paint circlePaint;
 
 
-    public int getCircleColor(){
+    public int getCircleColor() {
         return circleCol;
     }
 
-    public int getLabelColor(){
+    public int getLabelColor() {
         return labelCol;
     }
 
-    public String getLabelText(){
+    public String getLabelText() {
         return circleText;
     }
 
 
-    public void setCircleColor(int newColor){
+    public void setCircleColor(int newColor) {
         //update the instance variable
-        circleCol=newColor;
+        circleCol = newColor;
         //redraw the view
         invalidate();
         requestLayout();
     }
-    public void setLabelColor(int newColor){
+
+    public void setLabelColor(int newColor) {
         //update the instance variable
-        labelCol=newColor;
+        labelCol = newColor;
         //redraw the view
         invalidate();
         requestLayout();
     }
 
 
-    public void setLabelText(String newLabel){
+    public void setLabelText(String newLabel) {
         //update the instance variable
-        circleText=newLabel;
+        circleText = newLabel;
         //redraw the view
         invalidate();
         requestLayout();
     }
-    public LovelyView(Context context, AttributeSet attrs){
+
+    public LovelyView(Context context, AttributeSet attrs) {
         super(context, attrs);
         //paint object for drawing in onDraw
         circlePaint = new Paint();
@@ -74,7 +78,6 @@ public class LovelyView extends View {
         }
 
 
-
     }
 
 
@@ -82,27 +85,32 @@ public class LovelyView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         //get half of the width and height as we are working with a circle
-        int viewWidthHalf = this.getMeasuredWidth()/2;
-        int viewHeightHalf = this.getMeasuredHeight()/2;
+        int viewWidthHalf = this.getMeasuredWidth() / 2;
+        int viewHeightHalf = this.getMeasuredHeight() / 2;
         //get the radius as half of the width or height, whichever is smaller
 //subtract ten so that it has some space around it
         int radius = 0;
-        if(viewWidthHalf>viewHeightHalf)
-            radius=viewHeightHalf-10;
+        if (viewWidthHalf > viewHeightHalf)
+            radius = viewHeightHalf - 10;
         else
-            radius=viewWidthHalf-10;
+            radius = viewWidthHalf - 10;
 
         circlePaint.setStyle(Paint.Style.FILL);
         circlePaint.setAntiAlias(true);
         circlePaint.setColor(circleCol);
-        canvas.drawCircle(viewWidthHalf, viewHeightHalf, radius, circlePaint);
+        // canvas.drawCircle(viewWidthHalf, viewHeightHalf, radius, circlePaint);
+        Rect rect = new Rect();
+        rect.set(0, 0, 300, 200);
+        canvas.drawRect(rect, circlePaint);
         //set the text color using the color specified
         circlePaint.setColor(labelCol);
         //set text properties
         circlePaint.setTextAlign(Paint.Align.CENTER);
         circlePaint.setTextSize(50);
+        int xPos = (rect.width() / 2);
+        int yPos = (rect.height() / 2);
         //draw the text using the string attribute and chosen properties
-        canvas.drawText(circleText, viewWidthHalf, viewHeightHalf, circlePaint);
+        canvas.drawText(circleText, xPos, yPos, circlePaint);
 
     }
 }
